50c80ba2e3221956306305ca1238d8e51fb218f0
alejorom <morojela-github@yahoo.com>
2022-03-02 08:19:55 -0500

    Realizar los TODO 26/27/28

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -46,36 +46,44 @@ class StoryBrain {
     return _storyData[_storyNumber].choice2;
   }
 
-// Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it.
-int _storyNumber = 0;
+  // Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it.
+  int _storyNumber = 0;
 
-void nextStory(int choiceNumber)
-{
-  // Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user.
-  // e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
-  if (choiceNumber == 1 && _storyNumber == 0) {
-    _storyNumber = 2;
-  } else if (choiceNumber == 2 && _storyNumber == 0) {
-    _storyNumber = 1;
-  } else if (choiceNumber == 1 && _storyNumber == 1) {
-    _storyNumber = 2;
-  } else if (choiceNumber == 2 && _storyNumber == 1) {
-    _storyNumber = 3;
-  } else if (choiceNumber == 1 && _storyNumber == 2) {
-    _storyNumber = 5;
-  } else if (choiceNumber == 2 && _storyNumber == 2) {
-    _storyNumber = 4;
+  void nextStory(int choiceNumber)
+  {
+    // Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user.
+    // e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
+    if (choiceNumber == 1 && _storyNumber == 0) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 0) {
+      _storyNumber = 1;
+    } else if (choiceNumber == 1 && _storyNumber == 1) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 1) {
+      _storyNumber = 3;
+    } else if (choiceNumber == 1 && _storyNumber == 2) {
+      _storyNumber = 5;
+    } else if (choiceNumber == 2 && _storyNumber == 2) {
+      _storyNumber = 4;
+    }
+    else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
+      restart();
+    }
   }
-  else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
-    restart();
-  }
-}
 
-// Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
-void restart()
-{
-  _storyNumber = 0;
-}
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  // Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
+  void restart()
+  {
+    _storyNumber = 0;
+  }
+
+  // Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  bool buttonShouldBeVisible(){
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 }
